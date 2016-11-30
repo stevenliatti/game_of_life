0fb9fed171cfc6e420418cd45901f0fa58a2f822
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2016-11-30 14:15:06 +0100

    Changes to be committed: 	modified:   src/gameoflife.c 	modified:   src/makefile 	new file:   src/workers_management.c 	new file:   src/workers_management.h

---
+++ src/workers_management.h
@@ -0,0 +1,26 @@
+#ifndef _WORKERS_MANAGEMENT_H_
+#define _WORKERS_MANAGEMENT_H_
+
+typedef struct square_st {
+	bool is_alive;
+	bool is_alive_past;
+	int nb_neighbours;
+} square_t;
+
+typedef struct board_st {
+	square_t** matrix;
+	int width;
+	int height;
+} board_t;
+
+typedef struct worker_st {
+	board_t* board;
+	int workers_nb;
+	int id;
+} worker_t;
+
+void init_matrix(square_t** matrix);
+void init_board(board_t* board, square_t** matrix, int width, int height);
+void init_workers(worker_t* workers, int workers_nb, board_t* board);
+
+#endif
