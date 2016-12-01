1c3c73d8033343c969c58b4fe5f15c69dd033ba0
raed.abdennad <raed.abdennadher@etu.hesge.ch>
2016-11-30 16:14:55 -0800

    J'ai réussi à faire marcher le thread qui détecte la pression sur "Echap" et le thread qui affiche la board. Pour cela, j'ai utilisé une sémaphore qui permet de garantir que la création de l'environnement graphique (ctxt) se fasse avant le lancement du thread qui détecte la pression. sans cette sémaphore, on aura une "erreur de ségmentation" ce qui est logique, d'ailleurs d'après ce que Steven a dit, et ce qui est juste, que l'événnement de la pression est lié au contexte graphique.

--- src/test_raed/gfx_example.c
+++ src/test_raed/gfx_example.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include "gfx.h"
 
 #define SCREEN_WIDTH  800
 #define SCREEN_HEIGHT 600
-
 #define CHECK_ERR(expr, msg) if (expr) { fprintf(stderr, "%s\n", msg); return EXIT_FAILURE; }
 
-pthread_barrier_t barrier; 
-bool escape_pressed;
-struct gfx_context_t *ctxt;
+typedef struct {
+	bool pressed;
+	sem_t sem;
+} sync_t;
 
 /// If a key was pressed, returns its key code (non blocking call).
 /// List of key codes: https://wiki.libsdl.org/SDL_Keycode
@@ -39,49 +40,55 @@ static void render(struct gfx_context_t *context) {
 	}
 }
 
+static void sync_init(sync_t* sync) {
+	sync->pressed = false;
+	sem_init(&sync->sem, 0, 0);
+}
+
 void* keypress_thread(void* arg){
-	while (keypress() != SDLK_ESCAPE) {
-		//sleep(1);
+	sync_t* b = (sync_t*) arg;
+	//wait for the graphical context to be built in "work" thread
+	sem_wait(&b->sem);
+	while (!b->pressed) {
+		b->pressed = keypress() == SDLK_ESCAPE;
 	}
-	escape_pressed = true;
-	pthread_barrier_wait(&barrier);
+	//pthread_barrier_wait(&barrier);
 }
 
 void* work(void* arg) {
-	printf("Hello\n");
-	while(!escape_pressed){
-		ctxt = gfx_create("Example", SCREEN_WIDTH, SCREEN_HEIGHT);
-		if (!ctxt) {
-			fprintf(stderr, "Graphic mode initialization failed!\n");
-			return EXIT_FAILURE;
+	sync_t* b = (sync_t*) arg;
+	struct gfx_context_t *ctxt = gfx_create("Example", SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (!ctxt) {
+		fprintf(stderr, "Graphic mode initialization failed!\n");
+	}else{
+		printf("Hello\n");
+		//notify "keypress_thread" that the graphical context is built
+		sem_post(&b->sem);
+		while (!b->pressed) {
+			render(ctxt);
+			gfx_present(ctxt);
 		}
-		render(ctxt);
-		gfx_present(ctxt);
+		gfx_destroy(ctxt);
 	}
 }
 
 /// Program entry point.
 /// @return the application status code (0 if success).
 int main() {
-	escape_pressed = false;
 
-	pthread_barrier_init(&barrier, NULL, 2);
-
-	
-	pthread_t th_stop;
-	CHECK_ERR(pthread_create(&th_stop, NULL, keypress_thread, NULL), "pthread_create failed!");
+	sync_t sync;
+	sync_init(&sync);
 
 	pthread_t t;
-	CHECK_ERR(pthread_create(&t, NULL, work, NULL), "pthread_create failed!");
-	
-	
-	pthread_barrier_wait(&barrier);
+	CHECK_ERR(pthread_create(&t, NULL, work, &sync), "pthread_create failed!");
+
+	pthread_t th_stop;
+	CHECK_ERR(pthread_create(&th_stop, NULL, keypress_thread, &sync), "pthread_create failed!");
 
 	CHECK_ERR(pthread_join(t, NULL), "pthread_join failed!");
 	CHECK_ERR(pthread_join(th_stop, NULL), "pthread_join failed!");
 
-	gfx_destroy(ctxt);
-
 	printf("Bye Bye\n");
+
 	return EXIT_SUCCESS;
 }
