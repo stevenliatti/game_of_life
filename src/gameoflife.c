a2de958dd778955628bf97af3ba2a9251dfd2804
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2016-12-07 15:43:56 +0100

    Gestion des mauvais argument fait

--- src/gameoflife.c
+++ src/gameoflife.c
@@ -28,6 +28,10 @@ int main(int argc, char** argv) {
 		int freq = atoi(argv[5]);
 		int workers_nb = atoi(argv[6]);
 
+		if (width < 3 || height < 3 || seed < 0 || (prob < 0 && prob > 1) || freq <= 0 || workers_nb <= 0) {
+			fprintf(stderr, "Bad argument value\n");
+			return EXIT_FAILURE;
+		}
 
 		int squares_nb = (width - 2) * (height - 2);
 		if (workers_nb > squares_nb) {
