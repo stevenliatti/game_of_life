49bf573dacc43b57b7743a4d30877e79a4f52787
raed.abdennad <raed.abdennadher@etu.hesge.ch>
2016-12-07 17:09:20 +0100

    J'ai terminé les commentaires des fichiers suivant :  Modifications qui seront validées : 	modifié:         display_board.c 	modifié:         gfx.c 	modifié:         keyboard_interrupt.c

--- src/keyboard_interrupt.c
+++ src/keyboard_interrupt.c
@@ -1,8 +1,25 @@
+/**
+ * @file keyboard_interrupt.c
+ * @brief key interruption context
+ *
+ * This file contains functions for the thread that detect the key press event
+ *
+ * @author Steven Liatti
+ * @author Orphée Antoniadis
+ * @author Raed Abdennadher
+ * @bug No known bugs.
+ * @date December 7, 2016
+ * @version 1.0
+ */
+
 #include "keyboard_interrupt.h"
 
-/// If a key was pressed, returns its key code (non blocking call).
-/// List of key codes: https://wiki.libsdl.org/SDL_Keycode
-/// @return the key that was pressed or 0 if none was pressed.
+/**
+ * This function detect all events and return the escape key press event
+ *
+ * @param void
+ * @return SDL_Keycode the pressed key code
+ */
 SDL_Keycode keypress() {
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
@@ -14,7 +31,10 @@ SDL_Keycode keypress() {
 }
 
 /**
+ * Function that called by the thread that will detect the escape key press
  *
+ * @param arg sync_t struct
+ * @return void
  */
 void* keypress_thread(void* arg) {
 	sync_t* sync = (sync_t*) arg;
